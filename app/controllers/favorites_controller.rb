ac02985996113976242b657535be92c389eaf650
AyumiM5 <mmmiray.555@gmail.com>
2021-10-02 07:35:20 +0000

    [Add]comment-jQuery

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,12 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
   end
   
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
   end
   
 end

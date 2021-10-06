39cbadf4210d6e1896ee36270934cef6813b7ef6
AyumiM5 <mmmiray.555@gmail.com>
2021-10-06 01:59:55 +0000

    [Fix]star

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,10 +8,9 @@ class Book < ApplicationRecord
   validates :body, presence: true, length: { maximum: 200 }
     
   validates :rate, numericality: {
-    only_integer: true,
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
-  }
+  }, presence: true
   
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?

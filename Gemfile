5e3a3bba25c0a8d91781202a7bd26b0ef83aa10d
AyumiM5 <mmmiray.555@gmail.com>
2021-10-09 06:45:55 +0000

    [Fix]category

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ gem "refile-mini_magick"
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13'
+
+gem 'ransack'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -58,7 +58,7 @@ class BooksController < ApplicationController
   private
   
   def book_params
-    params.require(:book).permit(:title, :body, :rate)
+    params.require(:book).permit(:title, :body, :rate, :category)
   end
   
 end
